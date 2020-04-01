1b0d3b07ced2ce0619f5a92588c24950fb2dc743
aleksandrasN <62575892+aleksandrasN@users.noreply.github.com>
2020-04-01 11:50:20 +0300

    fixin some stuff

--- FinalProject/components/Signup.js
+++ FinalProject/components/Signup.js
@@ -36,54 +36,7 @@ export default class Signup extends React.Component {
 	onChangeTextPassword = password => this.setState({ password });
 	onChangeTextName = name => this.setState({ name });
 
-	onImageUpload = async () => {
-		const { status: cameraRollPerm } = await Permissions.askAsync(
-			Permissions.CAMERA_ROLL
-		);
-		try {
-			// only if user allows permission to camera roll
-			if (cameraRollPerm === 'granted') {
-				let pickerResult = await ImagePicker.launchImageLibraryAsync({
-					allowsEditing: true,
-					aspect: [4, 3]
-				});
-				console.log(
-					'ready to upload... pickerResult json:' + JSON.stringify(pickerResult)
-				);
-
-				var wantedMaxSize = 150;
-				var rawheight = pickerResult.height;
-				var rawwidth = pickerResult.width;
-				var ratio = rawwidth / rawheight;
-				var wantedwidth = wantedMaxSize;
-				var wantedheight = wantedMaxSize / ratio;
-				// check vertical or horizontal
-				if (rawheight > rawwidth) {
-					wantedwidth = wantedMaxSize * ratio;
-					wantedheight = wantedMaxSize;
-				}
-				let resizedUri = await new Promise((resolve, reject) => {
-					ImageEditor.cropImage(
-						pickerResult.uri,
-						{
-							offset: { x: 0, y: 0 },
-							size: { width: pickerResult.width, height: pickerResult.height },
-							displaySize: { width: wantedwidth, height: wantedheight },
-							resizeMode: 'contain'
-						},
-						uri => resolve(uri),
-						() => reject()
-					);
-				});
-				let uploadUrl = await firebaseSDK.uploadImage(resizedUri);
-				this.setState({ avatar: uploadUrl });
-				await firebaseSDK.updateAvatar(uploadUrl);
-			}
-		} catch (err) {
-			console.log('onImageUpload error:' + err.message);
-			alert('Upload image error:' + err.message);
-		}
-	};
+	
 
 	render() {
 		return (
